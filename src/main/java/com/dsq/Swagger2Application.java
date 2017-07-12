c3ff7f6d883b0ebb2ac6db4bd76d88030b57f55b
DongShiqing <18640947990@163.com>
2017-07-12 22:05:55 +0800

    test

---
+++ src/main/java/com/dsq/Swagger2Application.java
@@ -0,0 +1,12 @@
+package com.dsq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Swagger2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Swagger2Application.class, args);
+	}
+}
